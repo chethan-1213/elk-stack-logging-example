9d5453d176cdd5dad8bc012b863ff3751028c06f
chethan-1213 <chethan.ks@hikeon.tech>
2023-03-09 10:31:04 +0530

    elk basic

--- src/main/java/com/javatechie/elk/User.java
+++ src/main/java/com/javatechie/elk/User.java
@@ -1,5 +1,7 @@
 package com.javatechie.elk;
 
+import java.util.Objects;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,4 +15,45 @@ public class User {
 
     private int id;
     private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id && Objects.equals(name, other.name);
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + "]";
+	}
+    
 }
